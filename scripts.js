e010b90c28885490678b3f34b9b765f2c047720e
agnaldo <gui.agnaldo@gmail.com>
2023-10-05 21:12:41 -0300

    fix ajuste urls api

--- scripts.js
+++ scripts.js
@@ -11,7 +11,7 @@ formulario.addEventListener('submit', async function(event){
 
 
 async function getFilme(name){
-    let request = await fetch(`http://www.omdbapi.com/?s=${name}&apikey=e6c43dc6`);
+    let request = await fetch(`https://www.omdbapi.com/?s=${name}&apikey=e6c43dc6`);
     let response = await request.json();
     let result = response.Search;
     table.innerHTML = '';
